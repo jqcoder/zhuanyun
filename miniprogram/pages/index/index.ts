6aacc30177f6e2a29bd62b687fb5d53ec9133c2c
jie <jiecoder@outlo0k.com>
2022-07-03 16:35:30 +0800

    init project

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,17 @@
+// index.ts
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+
+  },
+
+  onLoad() {
+//     project.private.config.json
+// miniprogram_npm
+// node_modules
+// .vscode
+
+  },
+
+})
